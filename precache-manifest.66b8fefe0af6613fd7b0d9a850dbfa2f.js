52c86f37a3098af9c75f194091273c1a363d2c12
ivanschuh <ivan_schuh@hotmail.com>
2022-08-03 20:56:58 -0300

    Updates

---
+++ precache-manifest.66b8fefe0af6613fd7b0d9a850dbfa2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3dd78e0b22fc2095ebc",
+    "url": "/vagadev/static/css/main.a2370764.chunk.css"
+  },
+  {
+    "revision": "c3dd78e0b22fc2095ebc",
+    "url": "/vagadev/static/js/main.d4a09919.chunk.js"
+  },
+  {
+    "revision": "f7b1deedc9895ac7775c",
+    "url": "/vagadev/static/js/runtime~main.7418854e.js"
+  },
+  {
+    "revision": "f552d986335d36151478",
+    "url": "/vagadev/static/js/2.df0be7ad.chunk.js"
+  },
+  {
+    "revision": "625cc6f30e32cad2fd02",
+    "url": "/vagadev/static/js/3.6b60409e.chunk.js"
+  },
+  {
+    "revision": "b63003c517891a3ab634bb7d88b4cef0",
+    "url": "/vagadev/index.html"
+  }
+];
